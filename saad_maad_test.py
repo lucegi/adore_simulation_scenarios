c6af8e47218948d07560710131c90d5f4d8e70e7
Marko Mizdrak <marko.mizdrak@dlr.de>
2025-01-16 15:17:27 +0100

    add map file for decision maker infra

--- saad_maad_test.py
+++ saad_maad_test.py
@@ -44,7 +44,7 @@ def generate_launch_description():
             parameters=[
                 {"v2x_id": 1},
                 {"debug_mode_active": True},
-                {"optinlc_route_following": True}, # 0 for Lane following, 1 for OptiNLC route following
+                {"optinlc_route_following": False}, # 0 for Lane following, 1 for OptiNLC route following
                 {"planner_settings_keys": [ "wheel_base",
                                            "lateral_weight",
                                            "heading_weight",
@@ -109,11 +109,11 @@ def generate_launch_description():
             package='simulated_vehicle',
             namespace='traffic_participant_2',
             executable='simulated_vehicle',
-            name='simulated_vehicle',
+            name='simulated_vehicle2',
             parameters=[
-                {"set_start_position_x": 604786.672},
-                {"set_start_position_y": 5797162.799},
-                {"set_start_psi": 1.22},
+                {"set_start_position_x": 604780.672},
+                {"set_start_position_y": 5797119.799},
+                {"set_start_psi": 2.22},
                 {"set_shape": [4.5, 2.0, 2.0]}, # length, width, height
                 {"controllable": True},
             ]
@@ -122,11 +122,11 @@ def generate_launch_description():
              package='decision_maker',
              namespace='traffic_participant_2',
              executable='decision_maker',
-             name='decision_maker',
+             name='decision_maker2',
              parameters=[
                  {"v2x_id": 2},
                  {"debug_mode_active": False},
-                 {"optinlc_route_following": True}, # 0 for Lane following, 1 for OptiNLC route following
+                 {"optinlc_route_following": False}, # 0 for Lane following, 1 for OptiNLC route following
                  {"planner_settings_keys": [ "wheel_base",
                                             "lateral_weight",
                                             "heading_weight",
@@ -148,7 +148,7 @@ def generate_launch_description():
              package='mission_control',
              namespace='traffic_participant_2',
              executable='mission_control',
-             name='mission_control',
+             name='mission_control2',
              parameters=[
                  {"R2S map file": os.path.abspath("assets/tracks/de_bs_borders_wfs.r2sr")},
                  {"goal_position_x" : 604791.697},
@@ -159,7 +159,7 @@ def generate_launch_description():
             package='trajectory_tracker',
             namespace='traffic_participant_2',
             executable='trajectory_tracker_node',
-            name='trajectory_tracker',
+            name='trajectory_tracker2',
             parameters=[
                 {"set_controller": 1}, # 0 for MPC, 1 for PID
                 {"controller_settings_keys": [ "kp_x",
@@ -191,6 +191,9 @@ def generate_launch_description():
             executable='decision_maker_infrastructure',
             name='decision_maker_infrastructure',
             parameters=[
+
+            {"R2S map file": os.path.abspath("assets/tracks/de_bs_borders_wfs.r2sr")},
+
             ]
         ),
      
