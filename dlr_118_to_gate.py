8216c61ce1c5319278f96322cb463aa0ccc345a9
Sanath <sanath.konthala@dlr.de>
2025-01-29 14:20:38 +0100

    added dlr 118 to gate scenario

---
+++ dlr_118_to_gate.py
@@ -0,0 +1,110 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+
+def generate_launch_description():
+        # Get the directory of this launch file
+    launch_file_dir = os.path.dirname(os.path.realpath(__file__))
+    map_image_folder = os.path.abspath(os.path.join(launch_file_dir, "../assets/maps/"))
+    map_folder = os.path.abspath(os.path.join(launch_file_dir, "../assets/tracks/"))
+    
+    return LaunchDescription([
+        Node(
+            package='foxglove_bridge',
+            namespace='global',
+            executable='foxglove_bridge',
+            name='foxglove_bridge',
+            output='screen',
+            parameters=[
+                {'port': 8765},
+            ],
+        ),
+        Node(
+            package='visualizer',
+            namespace='ego_vehicle',
+            executable='visualizer',
+            name='visualizer',
+            parameters=[
+                {"asset folder": map_image_folder}
+            ]
+        ),
+        Node(
+            package='simulated_vehicle',
+            namespace='ego_vehicle',
+            executable='simulated_vehicle',
+            name='simulated_vehicle',
+            parameters=[
+                {"set_start_position_x": 606450.573},
+                {"set_start_position_y": 5797277.124},
+                {"set_start_psi": 1.5},
+                {"controllable": True},
+            ]
+        ),
+        Node(
+            package='decision_maker',
+            namespace='ego_vehicle',
+            executable='decision_maker',
+            name='decision_maker',
+            parameters=[
+                {"debug_mode_active": True},
+                {"optinlc_route_following": True}, # 0 for Lane following, 1 for OptiNLC route following
+                {"planner_settings_keys": [ "wheel_base",
+                                           "lateral_weight",
+                                           "heading_weight",
+                                           "maximum_velocity",
+                                           "min_distance_to_vehicle_ahead",
+                                           "look_ahead_for_curvature",
+                                           "look_behind_for_curvature"]},
+
+               {"planner_settings_values": [ 2.7,
+                                               0.2,
+                                               0.02,
+                                               4.0,
+                                               7.0,
+                                               40.0,
+                                               20.0]}
+            ],
+        ),
+        Node(
+            package='mission_control',
+            namespace='ego_vehicle',
+            executable='mission_control',
+            name='mission_control',
+            parameters=[
+                {"R2S map file": map_folder + "/de_bs_borders_wfs.r2sr"},
+                {"goal_position_x" : 606471.04},
+                {"goal_position_y" : 5797161.11}
+            ]
+        ),
+       Node(
+           package='trajectory_tracker',
+           namespace='ego_vehicle',
+           executable='trajectory_tracker_node',
+           name='trajectory_tracker',
+           parameters=[
+               {"set_controller": 1}, # 0 for MPC, 1 for PID
+               {"controller_settings_keys": [ "kp_x",
+                                           "ki_x",
+                                           "velocity_weight",
+                                           "kp_y",
+                                           "ki_y",
+                                           "heading_weight",
+                                           "kp_omega",
+                                           "dt",
+                                           "steering_comfort"]},
+
+               {"controller_settings_values": [ 0.3,
+                                               0.02,
+                                               0.3,
+                                               0.25,
+                                               0.0,
+                                               0.3,
+                                               0.1,
+                                               0.05,
+                                               2.5]}
+           ],
+           #output={'both': 'log'},
+       ),
+
+    ]
+)
