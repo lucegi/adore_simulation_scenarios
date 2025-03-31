fcb6f152b24bed8efd583515f9ffcf25fe7b70ee
Mikkel Skov Maarssoe <mikkel.maarssoe@dlr.de>
2025-03-31 09:45:25 +0200

    update saad maad test to new structure

--- saad_maad_test.py
+++ saad_maad_test.py
@@ -3,11 +3,11 @@ from launch_ros.actions import Node
 import os
 
 def generate_launch_description():
+        # Get the directory of this launch file
     launch_file_dir = os.path.dirname(os.path.realpath(__file__))
     map_image_folder = os.path.abspath(os.path.join(launch_file_dir, "../assets/maps/"))
     map_folder = os.path.abspath(os.path.join(launch_file_dir, "../assets/tracks/"))
     vehicle_param = os.path.abspath(os.path.join(launch_file_dir, "../assets/vehicle_params/"))
-
 
     return LaunchDescription([
         Node(
@@ -19,24 +19,37 @@ def generate_launch_description():
             parameters=[
                 {'port': 8765},
                 {'send_buffer_limit' : 500000000}
-
             ],
+        ),
+        # ================ infrastructure  ==================
+        Node(
+            package='simulated_infrastructure',
+            namespace='infrastructure',
+            executable='simulated_infrastructure',
+            name='simulated_infrastructure',
+            parameters=[
+                {"infrastructure_position_x": 604790.672},
+                {"infrastructure_position_y": 5797129.799},
+                {"validity_polygon": [604750.672, 5797109.799,
+                                       604750.672, 5797160.799,
+                                       604799.672, 5797160.799,
+                                       604799.672, 5797109.799,] }
+            ]
         ),
         Node(
             package='visualizer',
-            namespace='global',
-            executable='visualizer',
+            namespace='infrastructure',
+            executable='infrastructure_visualizer_node',
             name='visualizer',
             parameters=[
                 {"asset folder": map_image_folder},
-                {"whitelist": ["ego_vehicle", "sim_vehicle_2", "infrastructure"]},
-                {"visualization_offset_x": 604702.898},
-                {"visualization_offset_y": 5797111.036},
+                {"visualize_infrastructure": True},
+                {"visualize_local_map": True},
+                {"visualize_validity_area": True},
+                {"visualize_map_image": True}, # Only one visualizer can show images
+                {"visualize_traffic_participants": True},
             ]
         ),
-
-        ############################################ Infrastructure ################################################
-        
        Node(
             package='decision_maker_infrastructure',
             namespace='infrastructure',
@@ -46,24 +59,43 @@ def generate_launch_description():
                 {"map file":  map_folder + "/de_bs_borders_wfs.r2sr"},
                 {"infrastructure_position_x": 604790.672},
                 {"infrastructure_position_y": 5797129.799},
-                {"debug_mode_active": True},
+                {"debug_mode_active": False},
                 {"validity_polygon": [604750.672, 5797109.799,
-                                       604750.672, 5797139.799,
-                                       604799.672, 5797139.799,
+                                       604750.672, 5797160.799,
+                                       604799.672, 5797160.799,
                                        604799.672, 5797109.799,] }
             ]
         ),
-        ########################################### first vehicle #########################################
+
+        # ================ VEHICLE 1 ========================
+        Node(
+            package='visualizer',
+            namespace='ego_vehicle',
+            executable='vehicle_visualizer_node',
+            name='visualizer',
+            parameters=[
+                {"asset folder": map_image_folder},
+                {"visualize_vehicle": False},
+                {"visualize_planned_trajectory": False},
+                {"visualize_route": False},
+                {"visualize_local_map": False},
+                {"visualize_goal_point": True},
+                {"visualize_map_image": False},
+                {"visualize_traffic_participants": False},
+             ],
+        ),
         Node(
             package='simulated_vehicle',
             namespace='ego_vehicle',
             executable='simulated_vehicle',
             name='simulated_vehicle',
             parameters=[
-                {"set_start_position_x": 604843.301},
-                {"set_start_position_y": 5797114.574},
-                {"set_start_psi": 3.1},
+                {"set_start_position_x": 604835.481},
+                {"set_start_position_y": 5797113.518},
+                {"set_start_psi": 3.14},
                 {"controllable": True},
+                {"id": 111}, # Make sure this is unique for each vehicle
+                {"v2x_id": 111},
                 {"vehicle_model_file" : vehicle_param + "/NGC.json"}
             ]
         ),
@@ -73,28 +105,12 @@ def generate_launch_description():
             executable='decision_maker',
             name='decision_maker',
             parameters=[
-                {"v2x_id": 5},
-                {"debug_mode_active": True},
+                {"debug_mode_active": False},
                 {"optinlc_route_following": False}, # 0 for Lane following, 1 for OptiNLC route following
-                {"only_follow_reference_trajectories": False},
-                {"planner_settings_keys": [ "wheel_base",
-                                           "lateral_weight",
-                                           "heading_weight",
-                                           "maximum_velocity",
-                                           "min_distance_to_vehicle_ahead",
-                                           "look_ahead_for_curvature",
-                                           "look_behind_for_curvature"]},
-
-               {"planner_settings_values": [ 2.7,
-                                               0.2,
-                                               0.02,
-                                               5.0,
-                                               10.0,
-                                               40.0,
-                                               20.0]},
                 {"vehicle_model_file" : vehicle_param + "/NGC.json"}
+
             ],
-            # output={'both': 'log'},
+            output={'both': 'log'},
         ),
         Node(
             package='mission_control',
@@ -103,8 +119,8 @@ def generate_launch_description():
             name='mission_control',
             parameters=[
                 {"map file": map_folder + "/de_bs_borders_wfs.r2sr"},
-                {"goal_position_x" : 604791.697},
-                {"goal_position_y": 5797180.0}
+                {"goal_position_x" : 604988.297},
+                {"goal_position_y": 5797111.0}
             ]
         ),
        Node(
@@ -133,98 +149,188 @@ def generate_launch_description():
                                                0.1,
                                                0.05,
                                                2.5]},
-                {"vehicle_model_file" : vehicle_param + "/NGC.json"}
+                {"vehicle_model_file" : vehicle_param + "/NGC.json"},
+
            ],
-        #    output={'both': 'log'},
+           #output={'both': 'log'},
        ),
 
         ########################################### second vehicle #########################################
-        
+
+        Node(
+            package='visualizer',
+            namespace='sim_vehicle_1',
+            executable='vehicle_visualizer_node',
+            name='visualizer',
+            parameters=[
+                {"asset folder": map_image_folder},
+                {"visualize_vehicle": False},
+                {"visualize_planned_trajectory": False},
+                {"visualize_route": False},
+                {"visualize_local_map": False},
+                {"visualize_goal_point": True},
+                {"visualize_map_image": False},
+             ],
+        ),
+        Node(
+            package='simulated_vehicle',
+            namespace='sim_vehicle_1',
+            executable='simulated_vehicle',
+            name='simulated_vehicle',
+            parameters=[
+                {"set_start_position_x": 604731.230},
+                {"set_start_position_y": 5797112.750},
+                {"set_start_psi": 0.0},
+                {"set_shape": [4.5, 2.0, 2.0]}, # length, width, height
+                {"controllable": True},
+                {"vehicle_id": 222},
+                {"v2x_id": 0},
+                {"vehicle_model_file" : vehicle_param + "/NGC.json"}
+            ]
+        ),
+        Node(
+            package='decision_maker',
+            namespace='sim_vehicle_1',
+            executable='decision_maker',
+            name='decision_maker',
+            parameters=[
+                {"debug_mode_active": False},
+                {"optinlc_route_following": False}, # 0 for Lane following, 1 for OptiNLC route following
+                {"vehicle_model_file" : vehicle_param + "/NGC.json"}
+
+            ],
+            output={'both': 'log'},
+        ),
+        Node(
+            package='mission_control',
+            namespace='sim_vehicle_1',
+            executable='mission_control',
+            name='mission_control',
+            parameters=[
+                {"map file": map_folder + "/de_bs_borders_wfs.r2sr"},
+                {"goal_position_x" : 604791.697},
+                {"goal_position_y": 5797180.0}
+            ]
+        ),
+       Node(
+           package='trajectory_tracker',
+           namespace='sim_vehicle_1',
+           executable='trajectory_tracker_node',
+           name='trajectory_tracker',
+           parameters=[
+               {"set_controller": 2}, # 0 for MPC, 1 for PID
+               {"controller_settings_keys": [ "kp_x",
+                                           "ki_x",
+                                           "velocity_weight",
+                                           "kp_y",
+                                           "ki_y",
+                                           "heading_weight",
+                                           "kp_omega",
+                                           "dt",
+                                           "steering_comfort"]},
+
+               {"controller_settings_values": [ 0.3,
+                                               0.02,
+                                               0.3,
+                                               0.25,
+                                               0.0,
+                                               0.3,
+                                               0.1,
+                                               0.05,
+                                               2.5]},
+                {"vehicle_model_file" : vehicle_param + "/NGC.json"},
+
+           ],
+           #output={'both': 'log'},
+       ),
+     ########################################### third vehicle #########################################
+
+
+        Node(
+            package='visualizer',
+            namespace='sim_vehicle_2',
+            executable='vehicle_visualizer_node',
+            name='visualizer',
+            parameters=[
+                {"asset folder": map_image_folder},
+                {"visualize_vehicle": False},
+                {"visualize_planned_trajectory": False},
+                {"visualize_route": False},
+                {"visualize_local_map": False},
+                {"visualize_goal_point": True},
+                {"visualize_map_image": False},
+             ],
+        ),
         Node(
             package='simulated_vehicle',
             namespace='sim_vehicle_2',
             executable='simulated_vehicle',
-            name='simulated_vehicle2',
+            name='simulated_vehicle',
             parameters=[
-                {"set_start_position_x": 604702.898},
-                {"set_start_position_y": 5797111.036},
-                {"set_start_psi": 0.0},
+                {"set_start_position_x": 604788.611},
+                {"set_start_position_y": 5797185.181},
+                {"set_start_psi": 5.0 },
                 {"set_shape": [4.5, 2.0, 2.0]}, # length, width, height
                 {"controllable": True},
+                {"vehicle_id": 333}, # Make sure this is unique for each vehicle
+                {"v2x_id": 333},
                 {"vehicle_model_file" : vehicle_param + "/NGC.json"}
             ]
         ),
-         Node(
-             package='decision_maker',
-             namespace='sim_vehicle_2',
-             executable='decision_maker',
-             name='decision_maker2',
-             parameters=[
-                 {"v2x_id": 2},
-                 {"debug_mode_active": True},
-                 {"optinlc_route_following": False}, # 0 for Lane following, 1 for OptiNLC route following
-                 {"only_follow_reference_trajectories": False},
-                 {"planner_settings_keys": [ "wheel_base",
-                                            "lateral_weight",
-                                            "heading_weight",
-                                            "maximum_velocity",
-                                            "min_distance_to_vehicle_ahead",
-                                            "look_ahead_for_curvature",
-                                            "look_behind_for_curvature"]},
-
-                {"planner_settings_values": [ 2.7,
-                                                0.2,
-                                                0.02,
-                                                5.0,
-                                                10.0,
-                                                40.0,
-                                                20.0]},
-                {"vehicle_model_file" : vehicle_param + "/NGC.json"}
-             ],
-            #  output={'both': 'log'},
-         ),
-         Node(
-             package='mission_control',
-             namespace='sim_vehicle_2',
-             executable='mission_control',
-             name='mission_control2',
-             parameters=[
-                {"map file": map_folder + "/de_bs_borders_wfs.r2sr"},
-                {"goal_position_x" : 604791.697},
-                {"goal_position_y": 5797180.0}
-             ]
-         ),
         Node(
-            package='trajectory_tracker',
+            package='decision_maker',
             namespace='sim_vehicle_2',
-            executable='trajectory_tracker_node',
-            name='trajectory_tracker2',
+            executable='decision_maker',
+            name='decision_maker',
             parameters=[
-                {"set_controller": 2}, # 0 for MPC, 1 for PID
-                {"controller_settings_keys": [ "kp_x",
-                                            "ki_x",
-                                            "velocity_weight",
-                                            "kp_y",
-                                            "ki_y",
-                                            "heading_weight",
-                                            "kp_omega",
-                                            "dt",
-                                            "steering_comfort"]},
+                {"debug_mode_active": False},
+                {"optinlc_route_following": False}, # 0 for Lane following, 1 for OptiNLC route following
+                {"vehicle_model_file" : vehicle_param + "/NGC.json"}
 
-               {"controller_settings_values": [ 0.3,
-                                                0.02,
-                                                0.3,
-                                                0.25,
-                                                0.0,
-                                                0.3,
-                                                0.1,
-                                                0.05,
-                                                2.5]},
-            {"vehicle_model_file" : vehicle_param + "/NGC.json"}
             ],
             # output={'both': 'log'},
         ),
+        Node(
+            package='mission_control',
+            namespace='sim_vehicle_2',
+            executable='mission_control',
+            name='mission_control',
+            parameters=[
+                {"map file": map_folder + "/de_bs_borders_wfs.r2sr"},
+                {"goal_position_x" : 604791.697},
+                {"goal_position_y": 5797180.0}
+            ]
+        ),
+       Node(
+           package='trajectory_tracker',
+           namespace='sim_vehicle_2',
+           executable='trajectory_tracker_node',
+           name='trajectory_tracker',
+           parameters=[
+               {"set_controller": 2}, # 0 for MPC, 1 for PID
+               {"controller_settings_keys": [ "kp_x",
+                                           "ki_x",
+                                           "velocity_weight",
+                                           "kp_y",
+                                           "ki_y",
+                                           "heading_weight",
+                                           "kp_omega",
+                                           "dt",
+                                           "steering_comfort"]},
 
-     
-     ]
+               {"controller_settings_values": [ 0.3,
+                                               0.02,
+                                               0.3,
+                                               0.25,
+                                               0.0,
+                                               0.3,
+                                               0.1,
+                                               0.05,
+                                               2.5]},
+                {"vehicle_model_file" : vehicle_param + "/NGC.json"},
+
+           ],
+           #output={'both': 'log'},
+       ),
+    ]
 )
